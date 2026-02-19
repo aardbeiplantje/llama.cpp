829c379c0afc276aea7abf92f7e2a0f060e8220b
Tim Aerts <aardbeiplantje@gmail.com>
2026-02-19 20:10:31 +0100

    run_llama.sh: run llama.cpp docker

---
+++ run_llama.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+HF_HOME=${HF_HOME:-$(pwd)/huggingface}
+MODELS_DIR=${MODELS_DIR:-$(pwd)/models}
+DOCKER_IMAGE=${DOCKER_IMAGE:-local/ai/llama.cpp:latest}
+CONTAINER_NAME=${CONTAINER_NAME:-llama.cpp}
+extra_args=""
+if [ ! -z "$MODELS_PRESETS" ]; then
+    extra_args="$extra_args -v $MODELS_PRESETS:/llama/llamacpp_presets.ini:ro"
+fi
+exec docker run --rm \
+    --detach \
+    --name ${CONTAINER_NAME} \
+    -p 8000:8000 \
+    -v $HF_HOME:/hf:ro \
+    -v $MODELS_DIR:/models:ro \
+    $extra_args \
+    --memory=128g \
+    --device=/dev/kfd \
+    --device=/dev/dri \
+    --group-add=video \
+    --ipc=host \
+    --shm-size=128GB \
+    --cap-add=SYS_PTRACE \
+    --security-opt seccomp=unconfined \
+    --group-add=109 \
+    --group-add 992 \
+        $DOCKER_IMAGE
+
