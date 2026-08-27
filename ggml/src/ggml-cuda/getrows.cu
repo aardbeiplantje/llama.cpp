1af845c98368d02e7d0334ad2782bf52005acdc2
Tim Aerts <aardbeiplantje@gmail.com>
2026-08-27 15:48:48 +0000

    Fix: add Q8_0 support for HIP in get_rows_cuda

--- ggml/src/ggml-cuda/getrows.cu
+++ ggml/src/ggml-cuda/getrows.cu
@@ -345,10 +345,18 @@ static void ggml_cuda_get_rows_switch_src0_type(
             get_rows_cuda_q<QK5_1, QR5_1, dequantize_q5_1>(src0_d, src1_d, dst_d,
                 ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
             break;
+#if !defined(GGML_USE_HIP)
         case GGML_TYPE_Q8_0:
             get_rows_cuda_q<QK8_0, QR8_0, dequantize_q8_0>(src0_d, src1_d, dst_d,
                 ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
             break;
+#endif
+#if defined(GGML_USE_HIP)
+        case GGML_TYPE_Q8_0:
+            get_rows_cuda_q<QK8_0, QR8_0, dequantize_q8_0>(src0_d, src1_d, dst_d,
+                ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
+            break;
+#endif
         case GGML_TYPE_Q2_K:
             get_rows_cuda_kq<64, dst_t, dequantize_q2_K<dst_t>>(src0_d, src1_d, dst_d,
                 ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
